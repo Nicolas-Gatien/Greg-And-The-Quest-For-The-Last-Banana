00468185e5107b84d899c8901dfbfc0a55287a4b
polarcube1 <polarcube1@gmail.com>
2021-07-05 22:33:13 -0400

    Quick Backup
    About to rewrite the player movement script and I want a backup.

---
+++ GregNDaBoyzSearchFerDaLastBanana/Assets/Scripts/Player/Behaviors/IOnSpacePressedBehavior.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IOnSpacePressedBehavior
+{
+    public void SpaceUpdate();
+    public void SpacePressed();
+}
